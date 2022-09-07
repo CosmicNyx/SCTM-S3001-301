85127e7c5d0b082571b769431ad0e90e4fb8912d
CosmicNyx <ushnak2002@gmail.com>
2022-09-07 18:03:27 -0400

    Create class1.js

---
+++ class1.js
@@ -0,0 +1,18 @@
+// intro to wtv is going on rn im so tired
+
+console.log("Hello World!");
+
+
+//       '', {}, undefined, null ------------ means nothing ,,,,,, null refers to objects
+//  0, -0, NaN, Infinity, true false ,,, function
+
+//          function first () {}
+
+
+first("hello world");
+
+var x;
+
+//ik this stuff
+//global scope
+//inside can se out, outside cant see in.    --hidden--
