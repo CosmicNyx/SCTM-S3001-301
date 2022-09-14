09fa1e70011a1417adc6f56ccb31629d8403f196
CosmicNyx <ushnak2002@gmail.com>
2022-09-14 18:01:24 -0400

    thing

--- class2Arrays.js
+++ class2Arrays.js
@@ -27,7 +27,35 @@ console.log("--------");
 x.forEach(console.log);
 
 
+
+console.log("--------");
 // read file sync
 const fs = require('fs');
+let data = fs.readFileSync('test.csv');
+//hex
+console.log(data)
+//hex to string
+data = String(data);
+console.log(data)
+console.log(typeof data)
 
+//convert csv list to array of strings
+let dataarray = data.split(',');
 
+//sort string array ******
+console.log('sort string array')
+// dataarray.sort();
+// console.log(dataarray)
+
+console.log(typeof dataarray)
+console.log(dataarray)
+
+//convert it to a number array
+for(let i = 0; i<dataarray.length; i++){
+    dataarray[i] = Number(dataarray[i]);
+}
+console.log(dataarray)
+
+//sorted array
+dataarray.sort();
+console.log(dataarray)
