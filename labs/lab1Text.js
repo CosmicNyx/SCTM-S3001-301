01eea7d5d7412120f1a6b6551e714995dfa110cf
CosmicNyx <ushnak2002@gmail.com>
2022-09-20 13:26:01 -0400

    max and min

--- labs/lab1Text.js
+++ labs/lab1Text.js
@@ -1,51 +1,44 @@
-//break apart string
-// do that for loop from eamples
-// do plus 1 and keep it in another variable.
-// do same thing with numbers
-// make one witch index of just incase
-// should be fun
-// do string function today
 
-//reading file
+
+// -----------reading file-------
 
 const fs = require('fs');
 let data = fs.readFileSync('labs/testtext1.txt');
 
-console.log(typeof data)
-console.log("--------");
+// console.log(typeof data)
+// console.log("--------");
 //converting file to string
+
 data = String(data);
-console.log(data)
-console.log("--------");
-console.log(typeof data)
+
+// console.log(data)
+// console.log("--------");
+// console.log(typeof data)
 
 
-//
+//----Function
 
 var x = 0;
 var counter = 0;
 var word
 
-function findLongestWord(str) {
+function findLongestWord(string) {
 
-    var stringSplit = str.split(' ');
+    var splitWord = string.split(' ');
 
-    for (var i = 0; i < stringSplit.length; i++) {
+    for (var i = 0; i < splitWord.length; i++) {
 
-
-        if (stringSplit[i].length > x) {
-            console.log("--------");
-            console.log(stringSplit[i])
-
-            x = stringSplit[i].length;
+        if (splitWord[i].length > x) {
+            x = splitWord[i].length;
             counter = i;
-            word = stringSplit[i];
+            word = splitWord[i];
         }
     }
     return x;
+    
 }
+
 findLongestWord(data);
 
-console.log("--------");
-console.log(word)
-console.log(counter)
+console.log("The longest word here is " + word)
+console.log("The index of this word in the array is " + counter)
---
+++ labs/minMax.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+let data = fs.readFileSync('labs/testnumbers1.csv');
+
+
+data = String(data);
+
+//convert csv list to array of strings
+let dataarray = data.split(',');
+
+
+//convert it to a number array
+for(let i = 0; i<dataarray.length; i++){
+    dataarray[i] = Number(dataarray[i]);
+}
+
+
+
+console.log(dataarray.length)
+
+
+var largest = dataarray[0];
+var largeIndex = 0;
+
+var smallest = dataarray[0];
+var smallIndex = 0;
+
+//Largest num
+for (var i = 0; i < dataarray.length; i++) {
+    if (largest < dataarray[i] ) {
+        largest = dataarray[i];
+        largeIndex = i;
+    }
+}
+console.log("Largest " + largest);
+console.log("index " + largeIndex);
+
+//smallest num
+for (var i = 0; i < dataarray.length; i++) {
+    if (smallest > dataarray[i] ) {
+        smallest = dataarray[i];
+        smallIndex = i;
+    }
+}
+console.log("Smallest " + smallest);
+console.log("index " + smallIndex);
