f43da50dbe683a2a7f5c41a7a8c597f6ec679f51
CosmicNyx <ushnak2002@gmail.com>
2022-09-21 20:10:35 -0400

    remove console log

--- labs/minMax.js
+++ labs/minMax.js
@@ -28,15 +28,8 @@ var index = 0;
 
 //Largest num
 for (var i = 0; i < dataarray.length; i++) {
-    //used to see the 2 numbers that are being compared
-    console.log("----------")
-    console.log(dataarray[i])
-    console.log(largest)
-    console.log("----------")
     if (largest < dataarray[i] ) {
         largest = dataarray[i];
-        //used to see the new largest
-        console.log("new largest + "+largest)
         index = i;
     }
 }
@@ -46,15 +39,8 @@ console.log("index: " + index);
 
 //smallest num
 for (var i = 0; i < dataarray.length; i++) {
-    //used to see the 2 numbers that are being compared
-    console.log("----------")
-    console.log(dataarray[i])
-    console.log(smallest)
-    console.log("----------")
     if (smallest > dataarray[i] ) {
         smallest = dataarray[i];
-        //used to see new smallest
-        console.log("new smallest + "+smallest)
         index = i;
     }
 }
